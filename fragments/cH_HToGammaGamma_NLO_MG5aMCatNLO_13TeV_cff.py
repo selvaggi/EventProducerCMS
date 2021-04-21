4c9f44856f382f15d34b568f74993aa45961af55
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2021-04-22 00:47:09 +0200

    added cH production fragment

---
+++ fragments/cH_HToGammaGamma_NLO_MG5aMCatNLO_13TeV_cff.py
@@ -0,0 +1,46 @@
+import FWCore.ParameterSet.Config as cms
+
+# link to card:
+# https://github.com/cms-sw/genproductions/tree/22d41417a3f67c8065e7bb50997b39223d8f1223/bin/MadGraph5_aMCatNLO/cards/production/2017/13TeV/ChargedHiggs_hw
+
+externalLHEProducer = cms.EDProducer("ExternalLHEProducer",
+    args = cms.vstring('/eos/cms/store/cmst3/user/selvaggi/GridPacks/hc_5f_NLO_slc7_amd64_gcc700_CMSSW_10_6_19_tarball.tar.xz'),
+    nEvents = cms.untracked.uint32(5000),
+    numberOfParameters = cms.uint32(1),
+    outputFile = cms.string('cmsgrid_final.lhe'),
+    scriptName = cms.FileInPath('GeneratorInterface/LHEInterface/data/run_generic_tarball_cvmfs.sh')
+)
+
+from Configuration.Generator.Pythia8CommonSettings_cfi import *
+from Configuration.Generator.PSweightsPythia.PythiaPSweightsSettings_cfi import *
+from Configuration.Generator.Pythia8aMCatNLOSettings_cfi import *
+from Configuration.Generator.MCTunes2017.PythiaCP5Settings_cfi import *
+
+
+generator = cms.EDFilter("Pythia8HadronizerFilter",
+    maxEventsToPrint = cms.untracked.int32(1),
+    pythiaPylistVerbosity = cms.untracked.int32(1),
+    filterEfficiency = cms.untracked.double(1.0),
+    pythiaHepMCVerbosity = cms.untracked.bool(False),
+    comEnergy = cms.double(13000.),
+    PythiaParameters = cms.PSet(
+        pythia8CommonSettingsBlock,
+        pythia8PSweightsSettingsBlock,
+        pythia8CP5SettingsBlock,
+        pythia8aMCatNLOSettingsBlock,
+        processParameters = cms.vstring(
+            #'TimeShower:nPartonsInBorn = 2', #number of coloured particles (before resonance decays) in born matrix element
+            '25:m0 = 125.0',
+            '25:onMode = off',
+            '25:onIfMatch = 22 22',
+        ),
+        parameterSets = cms.vstring('pythia8CommonSettings',
+                                    'pythia8PSweightsSettings',
+                                    'pythia8CP5Settings',
+                                    'pythia8aMCatNLOSettings',
+                                    'processParameters',
+                                    )
+    )
+)
+
+ProductionFilterSequence = cms.Sequence(generator)
